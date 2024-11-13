a4ab26ef87f16f763eb7325c5d15cecbc862d6ca
Julian Fernando Camacho Calle <u22242383@utp.edu.pe>
2024-11-12 23:26:48 -0500

    CAMBIOS

--- Backend/src/main/java/com/example/DonnaPizza/webConfiguraciones/webConfig.java
+++ Backend/src/main/java/com/example/DonnaPizza/webConfiguraciones/webConfig.java
@@ -1,10 +1,10 @@
 package com.example.DonnaPizza.webConfiguraciones;
 
 import java.util.Locale;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
