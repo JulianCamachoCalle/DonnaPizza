b8241735f75486db3d699616746c5a9138e871df
Julian Fernando Camacho Calle <u22242383@utp.edu.pe>
2024-11-14 14:18:24 -0500

    BACKEND - PIZZAS

--- Backend/src/main/java/com/example/DonnaPizza/MVC/Pizzas/PizzasControlador.java
+++ Backend/src/main/java/com/example/DonnaPizza/MVC/Pizzas/PizzasControlador.java
@@ -1,65 +1,46 @@
 package com.example.DonnaPizza.MVC.Pizzas;
 
-import java.util.List;
-import java.util.Optional;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+@AllArgsConstructor
 @RestController
 @RequestMapping(path = "api/v1/pizzas")
-@CrossOrigin(origins = {"http://localhost:4200"})
 public class PizzasControlador {
 
-    // Link al Servicio
-    private final ServicioPizzas servicioPizzas;
+    private final PizzasService pizzasService;
 
-    @Autowired
-    public PizzasControlador(ServicioPizzas servicioPizzas) {
-        this.servicioPizzas = servicioPizzas;
-    }
-
-    // Obtener Todos
+    // Obtener todos
     @GetMapping
-    public List<Pizzas> getPizzas() {
-        return this.servicioPizzas.getPizzas();
+    Iterable<Pizzas> list() {
+        return pizzasService.findAll();
     }
 
-    // Obtener por Id
-    @GetMapping("{pizzasId}")
-    public ResponseEntity<Pizzas> getPizza(@PathVariable("pizzasId") Long id) {
-        Optional<Pizzas> pizzas = this.servicioPizzas.getPizzaById(id);
-        if (pizzas.isPresent()) {
-            return ResponseEntity.ok(pizzas.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    // Obtener por ID
+    @GetMapping("{id_pizza}")
+    public Pizzas get(@PathVariable Long id_pizza) {
+        return pizzasService.findById(id_pizza);
     }
 
-    // Registrar Nuevo
+    // Agregar
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public ResponseEntity<Object> registrarPizza(@RequestBody Pizzas pizza) {
-        return this.servicioPizzas.newPizza(pizza);
+    public Pizzas create(@RequestBody Pizzas pizza) {
+        return pizzasService.create(pizza);
     }
 
     // Actualizar
-    @PutMapping("/{id}")
-    public ResponseEntity<Object> actualizarPizza(@PathVariable Long id, @RequestBody Pizzas pizza) {
-        return this.servicioPizzas.updatePizza(id, pizza);
+    @PutMapping("{id_pizza}")
+    public Pizzas update(@PathVariable Long id_pizza, @RequestBody Pizzas pizza) {
+        return pizzasService.update(id_pizza, pizza);
     }
 
+
     // Eliminar
-    @DeleteMapping(path = "{pizzasId}")
-    public ResponseEntity<Object> eliminarPizza(@PathVariable("pizzasId") Long id) {
-        return this.servicioPizzas.deletePizza(id);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id_pizza}")
+    public void delete(@PathVariable Long id_pizza) {
+        pizzasService.delete(id_pizza);
     }
 }
